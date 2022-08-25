f28c3ff89f42d348d90d5c85d32b94ba7b4887f6
felipeazs <felipeazs@gmail.com>
2022-08-25 08:41:14 -0400

    dynamic display, output variables, map and objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 
 const App = () => {
+	const title = 'Blog post';
+	const body = 'This is my blog post';
+	const comments = [
+		{
+			id: 1,
+			text: 'comment one',
+		},
+		{
+			id: 2,
+			text: 'comment two',
+		},
+		{
+			id: 3,
+			text: 'comment three',
+		},
+	];
+
 	return (
 		<div className='container'>
-			<h1>Hello from the App component</h1>
+			<h1>{title}</h1>
+			<p>{body}</p>
+
+			<div className='comments'>
+				<h3>Comments({comments.length})</h3>
+				<ul>
+					{comments.map((comment) => (
+						<li key={comment.id}>{comment.text}</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
