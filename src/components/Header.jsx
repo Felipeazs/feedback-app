96c4caedacda77476fa8014baa67d30ac82bb10f
felipeazs <felipeazs@gmail.com>
2022-08-25 09:18:00 -0400

    Styling index.css. styles as default props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Header = ({ text }) => {
+const Header = ({ text, bgColor, textColor }) => {
+	const headerStyles = {
+		backgroundColor: bgColor,
+		color: textColor,
+	};
 	return (
-		<header>
+		<header style={headerStyles}>
 			<div className='container'>
 				<h2>{text}</h2>
 			</div>
@@ -14,11 +18,15 @@ const Header = ({ text }) => {
 //In case no props is passed, set default props:
 Header.defaultProps = {
 	text: 'Feedback Ui',
+	bgColor: 'rgba(0, 0, 0, 0.4)',
+	textColor: '#ff6a95',
 };
 
 //Not very used defining strict types
 Header.propTypes = {
 	text: PropTypes.string,
+	bgColor: PropTypes.string,
+	textColor: PropTypes.string,
 };
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App name='My Feedback App' />);
+root.render(<App name='My App' />);
